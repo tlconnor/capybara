0a7e376eeaf16ad9eb788b11ffb6832c39e447c8
Thomas Walpole <twalpole@gmail.com>
2016-08-31 15:34:40 -0700

    Not supporting RSpec 2.x anymore

--- lib/capybara/rspec/matchers.rb
+++ lib/capybara/rspec/matchers.rb
@@ -14,10 +14,6 @@ module Capybara
         end
       end
 
-      # RSpec 2 compatibility:
-      def failure_message_for_should; failure_message end
-      def failure_message_for_should_not; failure_message_when_negated end
-
       private
 
       def wrap_matches?(actual)
@@ -189,10 +185,6 @@ module Capybara
       def failure_message_when_negated
         "expected #{@window.inspect} not to become closed after #{@wait_time} seconds"
       end
-
-      # RSpec 2 compatibility:
-      alias_method :failure_message_for_should, :failure_message
-      alias_method :failure_message_for_should_not, :failure_message_when_negated
     end
 
     def have_selector(*args, &optional_filter_block)
